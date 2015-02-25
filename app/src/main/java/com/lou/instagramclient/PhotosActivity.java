f9db721680665569bde7350da6ec7d41e12bd6bc
Lou de los Reyes <loudlr@yahoo.com>
2015-02-25 13:00:26 -0800

    Initial checkin for InstagramClient

---
+++ app/src/main/java/com/lou/instagramclient/PhotosActivity.java
@@ -0,0 +1,47 @@
+package com.lou.instagramclient;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class PhotosActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photos);
+
+        // Send out Instagram API request to Popular Photos
+        fetchPopularPhotos();
+    }
+
+    // Trigger API request to fetch popular photos from Instagram
+    public void fetchPopularPhotos() {
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_photos, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
